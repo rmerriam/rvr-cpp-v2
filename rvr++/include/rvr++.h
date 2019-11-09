f3cecde8341e6cecb62fc4f4451ba0bae5d78ea8
rmerriam <rmerriam@mysticlakelinux>
2019-11-08 20:41:07 -0600

    reorg test

---
+++ rvr++/include/rvr++.h
@@ -0,0 +1,29 @@
+#ifndef RVR___H_
+#define RVR___H_
+//======================================================================================================================
+// 2019 Copyright Mystic Lake Software
+//
+// This is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// This is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//======================================================================================================================
+//
+//     Author: rmerriam
+//
+//     Created: Nov 8, 2019
+//
+//======================================================================================================================
+
+#include "ApiShell.h"
+#include "CommandBase.h"
+
+#endif /* RVR___H_ */
